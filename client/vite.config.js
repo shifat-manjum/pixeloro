8ec3db0f63effcb0580602b1331e9423dfc42af5
shifat-manjum <khshifat@gmail.com>
2026-09-04 16:18:32 +0200

    Perf: Implement JS code splitting and API preconnecting

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,4 +8,14 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          lucide: ['lucide-react']
+        }
+      }
+    }
+  }
 })
